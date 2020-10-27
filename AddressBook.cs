b4c1b2ed72a20285a45adbd25f0df4b7dc700653
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-27 09:56:19 +0530

    [tejas] add City count

--- AddressBook.cs
+++ AddressBook.cs
@@ -4,26 +4,26 @@ using System.Text;
 
 namespace AddressBookAssignment
 {
-  public class AddressBook
+    public class AddressBook
     {
-        List<Contact> list = new List<Contact>();
+        public HashSet<Contact> list = new HashSet<Contact>();
         public void Input()
         {
-            string[] details;
+            string[] details = new string[9];
 
             //// Taking input from user
             Console.WriteLine("Enter following details separated by ,");
             Console.WriteLine("First Name, Last Name, Address, City, State, pincode, phone, email address");
             details = Console.ReadLine().Split(",");
-            Contact c1 = new Contact(details[0], details[1], details[2], details[3], details[4], details[5], details[6], details[7]);
+            var c1 = new Contact(details[0], details[1], details[2], details[3], details[4], details[5], details[6], details[7]);
             list.Add(c1);
         }
         public void display()
         {
             foreach (Contact i in list)
             {
-                Console.WriteLine("First Name:" + i.fName + "Last Name:" + i.lName + "Address:" + i.address + "City:" + i.city
-                                   + "State:" + i.state + "pincode:" + i.phone + "phone: " + i.phone + "email address:" + i.email);
+                Console.WriteLine("First Name:" + i.fName + " Last Name:" + i.lName + " Address:" + i.address + " City:" + i.city
+                                   + " State:" + i.state + " pincode:" + i.phone + " phone: " + i.phone + " email address:" + i.email);
             }
         }
 
@@ -61,7 +61,7 @@ namespace AddressBookAssignment
                 }
             }
         }
-        public List<Contact> GetAddressBook()
+        public HashSet<Contact> GetAddressBook()
         {
             return list;
         }
