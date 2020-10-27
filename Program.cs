b1a6d86f989ba8fa87bac255c0461596cf6d9539
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-27 09:31:07 +0530

    [tejas] add Added Contact

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBookAssignment
 {
@@ -8,6 +9,38 @@ namespace AddressBookAssignment
         {
             Console.WriteLine("Welcome to Address Book!");
             Contact c1 = new Contact("rakesh", "sharma", "chinar park", "kolkata", "west bengal", "700157", "8017126325", "xyz@gmail.com");
+            int a = 1;
+            while (a == 1)
+            {
+                List<Contact> list = new List<Contact>();
+                Console.WriteLine("Enter your choice: 0.Add the data, 1.View the data");
+                int choice = int.Parse(Console.ReadLine());
+                AddressBook customer = new AddressBook();
+                switch (choice)
+                {
+                    case 0:
+                        customer.Input();
+                        break;
+                    case 1:
+                        customer.display();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the first name of contact to be edited");
+                        string first = Console.ReadLine();
+                        int check = customer.Edit(first);
+                        if (check == 0)
+                        {
+                            Console.WriteLine("Name not found");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter correct choice");
+                        break;
+                }
+            }
+
         }
     }
-}
+    }
+
